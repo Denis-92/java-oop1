bba062ea516f4fbb0960200645b32577ac6b4840
Denis-92 <d.mozenic@gmail.com>
2023-01-27 14:13:09 +0100

    init exercise Java Shop

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,26 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+	public static void main(String[] args) {
+		
+		// Creare la classe Prodotto che gestisce i prodotti dello shop.
+		// Un prodotto è caratterizzato da:
+		//		- codice (numero intero)
+		//		- nome
+		//		- descrizione
+		//		- prezzo
+		//		- iva
+		// Usate opportunamente i costruttori ed eventuali altri metodi di “utilità” per fare in modo che:
+		//		alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+		//		Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+		//		Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+		//	Nello stesso package aggiungete una classe Main con metodo main
+		//		nella quale testate tutte le funzionalità della classe Prodotto
+		
+		//	BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri
+		//		(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+
+	}
+
+}
