a41cc0f4e3edc34d636cf0b444f5be1db0fa736c
Denis-92 <d.mozenic@gmail.com>
2023-01-30 15:14:00 +0100

    add panel Prelievo e Versamento

--- src/jana60/bank/Bank.java
+++ src/jana60/bank/Bank.java
@@ -25,7 +25,40 @@ public class Bank {
 		);
 		
 		
+		int scelta;	
+		System.out.print("Scegliere: 1 per Versamento - 2 per Prelievo - 3 per Terminare: ");
+		scelta = receiveInput.nextInt();
+			
+		boolean versamento = scelta == 1;
+		boolean prelievo = scelta == 2;
+		boolean termina = scelta == 3;
 		
+		while (!termina) {
+			if (!versamento && !prelievo) {
+				System.out.println("Errore! Valore inserito non corretto!");
+				System.out.print("Scegliere: 1 per Versamento - 2 per Prelievo - 3 per Terminare: ");
+				scelta = receiveInput.nextInt();
+			}
+			if (versamento) {
+				System.out.print("Quanto vuoi versare sul conto? ");
+				float importoVersato = receiveInput.nextFloat();
+				contoCorrente.versamento(importoVersato);
+				System.out.print("Scegliere: 1 per Versamento - 2 per Prelievo - 3 per Terminare: ");
+				scelta = receiveInput.nextInt();
+			}
+			if (prelievo) {
+				System.out.print("Quanto vuoi prelevare dal conto? ");
+				float importoRichiesto = receiveInput.nextFloat();
+				contoCorrente.prelievo(importoRichiesto);
+				System.out.print("Scegliere: 1 per Versamento - 2 per Prelievo - 3 per Terminare: ");
+				scelta = receiveInput.nextInt();
+			}
+		}
+		
+		System.out.println("Programma terminato, grazie per aver usato il nostro servizio!");
+		
+		receiveInput.close();
+		// Chiuso inputContainer
 		
 	}
 
